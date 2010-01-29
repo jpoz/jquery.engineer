496fce05cbfb68b57ebb2a48ee3e97cbd1fff9ef
Revelation Ops <ops@revelationglobal.com>
2010-01-29 15:29:14 -0800

    adding in more examples

---
+++ example/js/buttons.js
@@ -0,0 +1,29 @@
+$.objects.define('button', {
+  defaults: { href:'#', text:'Click here', color:'green' },
+  structure: function(options) {
+    return $('<a/>', {
+      href: options.href,
+      'class': 'awesome ' + options.color,
+      html: options.text
+    });
+  }
+});
+
+$.objects.define('new_button_creator', {
+  behavior: function(self) {
+    self.
+    click(function() {
+        var text = $('#button_text').attr('value');
+        var color = $('#button_color').attr('value');
+
+        var new_button = $.objects.make('button', {color:color, text:text });
+        $('body').append(new_button);
+    })
+  }
+});
+
+$(document).ready(function() {
+  var new_button = $.objects.make('button', {color:'blue'}).behaveLike('new_button_creator');
+  $('body').append(new_button);
+});
+
